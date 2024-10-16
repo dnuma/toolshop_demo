873b3b3faead45a5abfbc3c409d0c47e2dcbe311
David Numa <david.numa@gmail.com>
2024-10-16 17:13:08 -0400

    Interface and Fill Form test created

--- src/lib/pages/contact.page.ts
+++ src/lib/pages/contact.page.ts
@@ -1,12 +1,39 @@
 import type { Page, Locator } from "@playwright/test";
 import { BasePage } from "./base.page";
+import { ContactForm } from "../interfaces/contactForm";
 
 export class ContactPage extends BasePage {
   readonly page: Page;
+  readonly fname: Locator;
+  readonly lname: Locator;
+  readonly email: Locator;
+  readonly subject: Locator;
+  readonly message: Locator;
+  readonly submitBtn: Locator;
 
   constructor(page: Page) {
     super(page);
     this.page = page;
+    this.fname = this.page.locator('[data-test="first-name"]');
+    this.lname = this.page.locator('[data-test="last-name"]');
+    this.email = this.page.locator('[data-test="email"]');
+    this.subject = this.page.locator('[data-test="subject"]');
+    this.message = this.page.locator('[data-test="message"]');
+    this.submitBtn = this.page.locator('[data-test="contact-submit"]');
+
+  }
+
+  async fillForm(userInfo: ContactForm) {
+    await this.fname.fill(userInfo.fname);
+    await this.lname.fill(userInfo.lname);
+    await this.email.fill(userInfo.email);
+    
+    await this.subject.click();
+    await this.subject.selectOption(userInfo.subject);
+
+    await this.message.fill(userInfo.message);
+    await this.submitBtn.click();
+
   }
 
 }
