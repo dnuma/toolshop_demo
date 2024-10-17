f5fc707f14ca47eeb8798a2bc7083cfc8732f141
David Numa <david.numa@gmail.com>
2024-10-17 16:04:50 -0400

    home page test created

--- src/lib/pages/home.page.ts
+++ src/lib/pages/home.page.ts
@@ -3,10 +3,17 @@ import { BasePage } from "./base.page";
 
 export class HomePage extends BasePage {
   readonly page: Page;
+  readonly sortTitle: Locator;
+  readonly filterTitle: Locator;
+  readonly banner: Locator;
 
   constructor(page: Page) {
     super(page);
     this.page = page;
+    this.sortTitle = this.page.getByRole('heading', { name: 'Sort' });
+    this.filterTitle = this.page.getByRole('heading', { name: 'Filters' });
+    this.banner = this.page.getByRole('img', { name: 'Banner' });
+
   }
 
   async goTo() {
