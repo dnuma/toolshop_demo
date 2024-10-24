d14eaf1ded62a5e04ce1cdc24ff592663ee674ee
David Numa <david.numa@gmail.com>
2024-10-24 15:19:54 -0400

    Other filters completed

--- src/tests/powertools.spec.ts
+++ src/tests/powertools.spec.ts
@@ -36,7 +36,61 @@ test.describe("Categories - Power tools testing",
         const resultsFiltered = results.filter((title) => title.includes(Category.Grinder));
         expect.soft(results).toEqual(resultsFiltered);
       }
-      
     })
+    
+    test("Filter by Sander", {
+      tag: "@sander",      
+    }, async( { page }) => {
+      const categoriesPage = new CategoriesPage(page);
+      const cardTitleLocator = categoriesPage.cardTitle;
+      
+      await categoriesPage.filterBy(Category.Sander);
+      
+      if(await categoriesPage.pageEmpty.isVisible()){
+        expect(true, { message: "There are no products found"});
+      }
+      else {
+        const results = await categoriesPage.getCardInfo(cardTitleLocator);
+        const resultsFiltered = results.filter((title) => title.includes(Category.Sander));
+        expect.soft(results).toEqual(resultsFiltered);
+      }
+    })
+
+    test("Filter by Saw", {
+      tag: "@saw",      
+    }, async( { page }) => {
+      const categoriesPage = new CategoriesPage(page);
+      const cardTitleLocator = categoriesPage.cardTitle;
+      
+      await categoriesPage.filterBy(Category.Saw);
+      
+      if(await categoriesPage.pageEmpty.isVisible()){
+        expect(true, { message: "There are no products found"});
+      }
+      else {
+        const results = await categoriesPage.getCardInfo(cardTitleLocator);
+        const resultsFiltered = results.filter((title) => title.includes(Category.Saw));
+        expect.soft(results).toEqual(resultsFiltered);
+      }
+    })
+
+    test("Filter by Drill", {
+      tag: "@drill",      
+    }, async( { page }) => {
+      const categoriesPage = new CategoriesPage(page);
+      const cardTitleLocator = categoriesPage.cardTitle;
+      
+      await categoriesPage.filterBy(Category.Drill);
+      
+      if(await categoriesPage.pageEmpty.isVisible()){
+        expect(true, { message: "There are no products found"});
+      }
+      else {
+        const results = await categoriesPage.getCardInfo(cardTitleLocator);
+        const resultsFiltered = results.filter((title) => title.includes(Category.Drill));
+        expect.soft(results).toEqual(resultsFiltered);
+      }
+    })
+
   }
 )
