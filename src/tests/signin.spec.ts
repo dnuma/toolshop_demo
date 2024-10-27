faf3774d73be68a43f216c255cd9eb1c087ddad0
David Numa <david.numa@gmail.com>
2024-10-27 00:01:30 -0400

    Refactor the sign in test

--- src/tests/signin.spec.ts
+++ src/tests/signin.spec.ts
@@ -16,20 +16,24 @@ test.describe(
     let email = "";
     let password = "";
 
-    test("Register the user", async ({ page }) => {
+    test("Register and Sign In user", async ({ page }) => {
       const signInPage = new SignInPage(page);
-      email = signInPage.generateRandomEmail();
-      password = signInPage.generateRandomPassword();
 
-      await signInPage.register(email, password);
-      await expect(signInPage.title).toBeVisible();
-    });
+      await test.step("Generate random user credentials", async () => {
+        email = signInPage.generateRandomEmail();
+        password = signInPage.generateRandomPassword();
+      });
 
-    test("Sign in the user", async ({ page }) => {
-      const signInPage = new SignInPage(page);
-      const fullName = `${process.env.FIRST_NAME} ${process.env.LAST_NAME}`;
-      await signInPage.signIn(email, password);
-      await expect(signInPage.navMenu).toContainText(fullName);
+      await test.step("Register the user", async () => {
+        await signInPage.register(email, password);
+        await expect(signInPage.title).toBeVisible();
+      });
+
+      await test.step("Sign in the user", async () => {
+        const fullName = `${process.env.FIRST_NAME} ${process.env.LAST_NAME}`;
+        await signInPage.signIn(email, password);
+        await expect(signInPage.navMenu).toContainText(fullName);
+      });
     });
   }
 );
